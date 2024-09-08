e7b81deaf1ccb0ec5f2c3364ad07d7587150a844
Rafiki <maachiirafik@gmail.com>
2024-09-08 19:16:42 +0200

    Avacencement avec quelque Problématique

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,6 @@
+<?php
+require("../config/commandes.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +8,71 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  <script src="/docs/5.3/assets/js/color-modes.js"></script>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.122.0">
 </head>
 
 <body>
+  <div class="album py-5 bg-body-tertiary">
+    <div class="container">
 
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <form method="$_POST" action="admin.php">
+          <div class="form-group">
+            <label for="exampleInputTitle1">Titre Image</label>
+            <input type="name" class="form-control" name="image" required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputName1">Nom du Produit</label>
+            <input type="text" class="form-control" name="nom" required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPrice1">Prix</label>
+            <input type="number" class="form-control" name="prix" required>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputDescription1">Description</label>
+            <input type="text-area" class="form-control" name="description" required>
+          </div>
+
+          <button type="submit" class="btn btn-primary" name="validez">Ajoute un nouveaux produit</button>
+        </form>
+
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
+
+<?php
+
+if (isset($_POST['validez'])) {
+  # code...
+  if (isset($_POST['image']) && isset($_POST['nom']) && isset($_POST['prix']) && isset($_POST['description'])) {
+    # code...
+    if (!empty($_POST['image']) && !empty($_POST['nom']) && !empty($_POST['prix']) && !empty($_POST['description'])) {
+      $image = htmlspecialchars(strip_tags($_POST['image']));
+      $nom = htmlspecialchars(strip_tags($_POST['nom']));
+      $prix = htmlspecialchars(strip_tags($_POST['prix']));
+      $description = htmlspecialchars(strip_tags($_POST['description']));
+      try {
+        //code...
+        ajouter($image, $nom, $prix, $description);
+        echo "Produit ajouté avec succès!";
+      } catch (\Throwable $th) {
+        //throw $th;
+        echo "Erreur : " . $th->getMessage();
+      }
+    }
+  }
+}
+?>
--- index.php
+++ index.php
@@ -177,14 +177,12 @@ $Produits = afficher();
         <div class="row">
           <div class="col-sm-8 col-md-7 py-4">
             <h4>About</h4>
-            <p class="text-body-secondary">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+            <p class="text-body-secondary">Un jeune bilel sauvage voulais devenir riche</p>
           </div>
           <div class="col-sm-4 offset-md-1 py-4">
-            <h4>Contact</h4>
+            <h4>Se Connecter</h4>
             <ul class="list-unstyled">
-              <li><a href="#" class="text-white">Follow on Twitter</a></li>
-              <li><a href="#" class="text-white">Like on Facebook</a></li>
-              <li><a href="#" class="text-white">Email me</a></li>
+              <li><a href="admin/index.php" class="text-white">Connexion</a></li>
             </ul>
           </div>
         </div>
@@ -193,11 +191,7 @@ $Produits = afficher();
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container">
         <a href="#" class="navbar-brand d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24">
-            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-            <circle cx="12" cy="13" r="4" />
-          </svg>
-          <strong>Album</strong>
+          <strong>Biloux On The flux</strong>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -216,7 +210,11 @@ $Produits = afficher();
           <?php foreach ($Produits as $produit): ?>
             <div class="col">
               <div class="card shadow-sm">
-                <title><?= $produit->nom ?></title>
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                  <title><?= $produit->nom ?></title>
+                  <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                </svg>
+                <title></title>
                 <img src="<?= $produit->image ?>">
                 <div class="card-body">
                   <p class="card-text"><?= substr($produit->description, 0, 200) ?></p>
